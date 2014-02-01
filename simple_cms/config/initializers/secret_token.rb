4a165d4b8b46942b1d04b6287e643a2d47de90e6
Brandon Morrison <brandon@brandonam.com>
2014-02-01 00:35:26 -0800

    opened simple_cms rails project with mysql

---
+++ simple_cms/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimpleCms::Application.config.secret_key_base = 'b24b25f110b0ab8b6e4e149783e2b7716a14a6957c142f4c770029c13daa6cfc69e601ff3af073c8e150dde71b13b3a08774962c310e0064350e32d35ebbb89b'
