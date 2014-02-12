ca5c99f9507df41baa9cc34eabff13bd297ea2a5
Brandon Morrison <brandon@brandonam.com>
2014-02-12 12:36:57 -0800

    end of ch 8

--- simple_cms/app/models/admin_user.rb
+++ simple_cms/app/models/admin_user.rb
@@ -1,4 +1,7 @@
 class AdminUser < ActiveRecord::Base
   #To configure a differnt table name
   #set_table_name("admin_users")
+  
+  has_and_belongs_to_many :pages
+  scope :named, lambda {|first,last| where(:first_name => first, :last_name => last)}
 end
--- simple_cms/app/models/subject.rb
+++ simple_cms/app/models/subject.rb
@@ -1,2 +1,9 @@
 class Subject < ActiveRecord::Base
+  
+  has_many :pages
+  
+  #obj.visible - returns visible ones
+  scope :visible, -> { where(:visible => true) }
+  scope :invisible, -> { where(:visible => false) }
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
